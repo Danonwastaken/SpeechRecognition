9106eb42c708ed6c610b919c9eb97c9c7779714e
Danonwastaken <looross@mail.ru>
2022-07-08 15:35:27 +0300

    удаление словаря и добавление команд

--- main.py
+++ main.py
@@ -1,69 +1,75 @@
-import pyttsx3
-import speech_recognition as sr
+import speech_recognition as sr 
+import webbrowser as wb 
+import os 
+import sys
+import time
+from Audio.sound import Sound
+from urllib.parse import quote_plus
 
+recog = sr.Recognizer()
+print("Здравствуйте! Я вас слушаю") # Приветствие 
 
-'''with microphone:
+def record(): #Запись голоса и перевод в текст
+    mic = sr.Microphone()
+    with mic:
         recognized_data = ""
-        print("Проверка микорофна. Скажите что-нибудь")
-        audio = rec.listen(microphone, None, 5)
-recognized_data = rec.recognize_google(audio, language="ru").lower()
-print("Вы сказали: " + recognized_data.lower())'''
+        audio = recog.listen(mic, 5, 5) 
+        print("Прослушивание завершено!")
+        try:
+            recognized_data = recog.recognize_google(audio, language="ru").lower() 
+            print(recognized_data)
+        except sr.UnknownValueError: #Если команда не разобрана
+            print("Команда не распознана") 
+        return recognized_data
 
-#Test
+def command(terms):  #Создание команды
+        for x in terms:
+            if x in recognized_data:
+                return True
 
-def record():
-        mic = sr.Microphone()
-        with mic:
-                recognized_data = ""
-                recog = sr.Recognizer()
-                #mic = sr.Microphone()
-                audio = recog.listen(mic, 5, 5)        
-                try:
-                        recognized_data = recog.recognize_google(audio, language="ru").lower()
-                        print("Вы сказали:" + recognized_data.lower())
-                except sr.WaitTimeoutError:
-                        print("Повторите команду ещё раз")
-                        return
-                except sr.UnknownValueError:
-                        pass
-                return recognized_data
+def commands(recognized_data): #Функция, отвечающая за команды
+    #З1 - Открытие диспетчера задач:
+    if command(["открой диспетчер задач"]):
+        print("Открываю...")
+        os.system("taskmgr")
 
+    #З2 - Открытие настроек операционной системы:
+    if command(["открой панель управления"]):
+        print("Сейчас открою") 
+        os.system("control")   
 
-'''
+    #З3 - Поиск в интернете:
+    if command(["поиск в интернете"]):
+        call = recognized_data.split(" ",3)[3]
+        wb.open_new_tab("https://yandex.ru/search/?text=" + quote_plus(call) + "&clid=2100784-306&banerid=6302000000%3A5e0887dc35a2d000254ec9d6&win=417&lr=37143")
+        print("Смотрите, что я вам смогла найти!")
 
-diction = {
-        name: ("кальцит", "кацит"),
-        commands: {
-                task_manager: ("диспетчер задач", "открой диспетчер задач"),
-                ctrl_panel: ("панель управления", "открой парель управления"),
-                browser_search: ("поиск в интернете", "найди в интернете", "поищи", "найди мне"),
-                shutdown: ("закройся", "выключись", "отдыхай"),
-                open_host: ("открой hosts", "открой файл hosts", "открой файл хостс", "открой хостс", "открой hosts в блокноте",
-                                "открой файл hosts в блокноте", "открой хостс в блокноте", "открой файл хостс в блокноте"),
-                volume_up: ("увеличь громкость на"),
-                volume_down: ("уменьши громкость на"),
-                multip: ("умножь на, умножь")
-        }
-        
-}
-''' 
-def speak(voice):
-        print(voice)   
-        speak_engine.say(voice)
-        speak_engine.runAndWait()
-        speak_engine.stop()
-speak_engine = pyttsx3.init()
+    #З4 - Закрытие голосового ассистента:
+    if command(["пока", "выключись", "увидимся"]):   
+        print("Пока!")
+        sys.exit()  
+     
+    #З5 - Открыть файл hosts в блокноте:
+    if command(["открой hosts", "открой файл hosts", "открой файл хостс", "открой хостс", "открой hosts в блокноте", "открой файл hosts в блокноте", "открой хостс в блокноте", "открой файл хостс в блокноте"]):
+        print("Секунду...")
+        os.system("Notepad C:/Windows/System32/drivers/etc/hosts")
 
-speak("Кальцит слушает")
+    #З6 - Установка громкости на N: 
+    if command(["поставь громкость", "установи громкость"]):
+        vol = int(recognized_data.split(" ",2)[2]) #Удаление первых двух слов
+        Sound.volume_set(vol)
 
-def main():
-        global recog
-        global recor
-        global mic
-        recog = sr.Recognizer()
-        mic = sr.Microphone()
-        recor = record()
+    #З7 - Умножение двух чисел
+    if command(["x"]):
+        znak = recognized_data.split(" ")[1] 
+        if znak == "x": 
+            print(int(recognized_data.split()[0]) * int(recognized_data.split()[2])) # Умножение первого и второго числа
+        else:
+            print("Не правильно назван знак!")
+    else:
+        print("Команда не найдена!")
 
-        
-if __name__ == '__main__':
-        record()
+# "Бесконечно" работающая программа
+while True:
+    recognized_data = record()
+    commands(recognized_data) 
